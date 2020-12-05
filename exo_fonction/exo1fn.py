7e3a40573424d6a9fea836e2351c69ebd15686c8
ismuch <ismaelmoutsinga@gmail.com>
2020-12-05 12:11:21 +0100

    Ajout exo boucle+fonctions+shell

---
+++ exo_fonction/exo1fn.py
@@ -0,0 +1,8 @@
+def fonction(x,y):
+	liste = []
+	a=0
+	while a<=y-x:
+		a=a+x
+		liste.append(a)
+	print(liste)
+fonction(3,20)
---
+++ exo_fonction/exo2fn.py
@@ -0,0 +1,8 @@
+x = 30
+def fonc(x,y,z):
+    somme = z*(x+y)
+    print (somme)
+    print (x)
+fonc(1,3,4)
+print(x)
+
