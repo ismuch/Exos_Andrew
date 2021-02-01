9fbbeb5e3c77ce339f1549d99b55efc7487a7a54
Ismael Moutsinga <ismaelmoutsinga@gmail.com>
2021-02-01 16:27:25 +0100

    ajouts dossier td4

---
+++ Exos_td/TD4/td4exo5.py
@@ -0,0 +1,15 @@
+def letitre(x) :
+	print(80*"=")
+	print(x)
+	print(80*"=")
+
+letitre("belle journée")
+
+
+def letitre2(x):
+	print(80*"=")
+	print("=",x.center(76),"=")
+	print(80*"=")
+
+
+letitre2("Voiture rouge")
